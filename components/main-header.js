264996126169b23554854b9c38f7620431465878
Gosia Magdzik <melassa26@gmail.com>
2024-03-20 19:29:39 +0100

    header layout

--- components/main-header.js
+++ components/main-header.js
@@ -1,9 +1,23 @@
 import Link from "next/link";
+import logoImg from '@/assets/logo.png'
 
 export default function MainHeader() {
     return <header>
         <Link href="/">
-            
+            <img src={logoImg.src} alt="Plate with food on it"/>
+            NextLevel Food
         </Link>
+        <nav>
+            <ul>
+                <li>
+                    <Link href="/meals">
+                        Browse Meals
+                    </Link>
+                    <Link href="/community">
+                        Foodies Navigation
+                    </Link>
+                </li>
+            </ul>
+        </nav>
     </header>
 }
