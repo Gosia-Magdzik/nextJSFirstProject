584e938472cd559328b69826b1426616489f6951
Gosia Magdzik <melassa26@gmail.com>
2024-03-22 16:29:58 +0100

    styles has been applied

--- components/main-header.js
+++ components/main-header.js
@@ -1,13 +1,15 @@
 import Link from "next/link";
-import logoImg from '@/assets/logo.png'
+import logoImg from '@/assets/logo.png';
+import classes from './main-header.module.css';
+
 
 export default function MainHeader() {
-    return <header>
-        <Link href="/">
+    return <header className={classes.header}>
+        <Link href="/" className={classes.logo}>
             <img src={logoImg.src} alt="Plate with food on it"/>
             NextLevel Food
         </Link>
-        <nav>
+        <nav className={classes.nav}>
             <ul>
                 <li>
                     <Link href="/meals">
