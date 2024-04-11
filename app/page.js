d7c530c2518c435f35d17b7b1caa7831981007db
Gosia Magdzik <melassa26@gmail.com>
2024-04-11 19:25:35 +0200

    populating The starting page content & CSS added

--- app/page.js
+++ app/page.js
@@ -1,14 +1,24 @@
 import Link from "next/link";
+import classes from "./page.module.css";
 
 export default function Home() {
   return (
-    <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
-        Time to get started!
-      </h1>
-      <p><Link href="/meals">Meals</Link></p>
-      <p><Link href="/meals/share">Meals</Link></p>
-      <p><Link href="/community">Community</Link></p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.slideshow}>
+
+        </div>
+        <div>
+          <div className={classes.hero}>
+            <h1>NextLevel Food for NextLevel Foodies</h1>
+          </div>
+          <div className={classes.cta}>
+
+          </div>
+        </div>
+      </header>
+      <main></main>
+    </>
   );
 }
+
