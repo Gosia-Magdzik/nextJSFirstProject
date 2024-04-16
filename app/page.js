3d90fe7fe5f2ab497380b17334f3e492e5126f35
Gosia Magdzik <melassa26@gmail.com>
2024-04-16 16:06:19 +0200

    image slideshow

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Link from "next/link";
+//import { useClient } from 'next/data-client';
 import classes from "./page.module.css";
+import ImageSlideshow from "@/components/main-header/images/image-slideshow";
 
 export default function Home() {
   return (
     <>
       <header className={classes.header}>
         <div className={classes.slideshow}>
-
+          <ImageSlideshow/>
         </div>
         <div>
           <div className={classes.hero}>
@@ -15,10 +17,36 @@ export default function Home() {
           </div>
           <div className={classes.cta}>
             <Link href="/community">Join the Community</Link>
+            <Link href="/meals">Explore Meals</Link>
           </div>
         </div>
       </header>
-      <main></main>
+      <main>
+        <section className={classes.section}>
+            <h2>How it works</h2>
+            <p>
+              NextLevel Food is a platform for foodies to share their favorite
+              recipes with the world. It&apos;s a place to discover new dishes, and to
+              connect with other food lovers.
+            </p>
+            <p>
+              NextLevel Food is a place to discover new dishes, and to connect
+              with other food lovers.
+            </p>
+          </section>
+          <section className={classes.section}>
+            <h2>Why NextLevel Food?</h2>
+            <p>
+              NextLevel Food is a platform for foodies to share their favorite
+              recipes with the world. It&apos;s a place to discover new dishes, and to
+              connect with other food lovers.
+            </p>
+            <p>
+              NextLevel Food is a place to discover new dishes, and to connect
+              with other food lovers.
+            </p>
+          </section>
+      </main>
     </>
   );
 }
