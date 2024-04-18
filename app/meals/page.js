98b397c844963169fd43ee4c5415c50e78eb5d05
Gosia Magdzik <melassa26@gmail.com>
2024-04-18 16:57:57 +0200

    fetching data, async await

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,9 +1,13 @@
 import MealsGrid from '@/components/meals/meals-grid';
 import classes from './page.module.css';
 import Link from 'next/link';
+import { getMeals } from '@/lib/meals';
 
-export default function Meals() {
-    return <>
+export default async function Meals() {
+    const meals = await getMeals();
+    
+    return (
+        <>
         <header className={classes.header}>
             <h1>
                 Delicious meals, created{' '}
@@ -19,7 +23,8 @@ export default function Meals() {
             </p>
         </header>
         <main className={classes.main}>
-            <MealsGrid meals={[]} />
+            <MealsGrid meals={meals} />
         </main>
-    </>
+        </>
+    )
 }
