bf5d4ca24b0acf0ad194efa9e75204888be0e890
Gosia Magdzik <melassa26@gmail.com>
2024-04-25 13:36:08 +0200

    streamed responses for granular loading state management

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,10 +2,14 @@ import MealsGrid from '@/components/meals/meals-grid';
 import classes from './page.module.css';
 import Link from 'next/link';
 import { getMeals } from '@/lib/meals';
+import { Suspense } from 'react';
 
-export default async function Meals() {
+async function Meals() {
     const meals = await getMeals();
-    
+    return  <MealsGrid meals={meals} />
+}
+
+export default  function MealsPage() {
     return (
         <>
         <header className={classes.header}>
@@ -23,7 +27,12 @@ export default async function Meals() {
             </p>
         </header>
         <main className={classes.main}>
-            <MealsGrid meals={meals} />
+            <Suspense   fallback = {
+                        <p className={classes.loading}>
+                        Fetching meals...
+                        </p>} >
+                <Meals/>
+            </Suspense>
         </main>
         </>
     )
