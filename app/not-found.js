e8d7aa9b0fd4eb0db9b85d9fa0fce7da5f369140
Gosia Magdzik <melassa26@gmail.com>
2024-04-30 16:42:53 +0200

    handling 'not found error'

---
+++ app/not-found.js
@@ -0,0 +1,6 @@
+export default function NotFound() {
+    return <main className="not-found">
+        <h1>NotFound</h1>
+        <p>Unfortunately, we could mot find the requested page or resource</p>
+    </main>
+}
