55435942c2b6f621188c3861dc8eefb96f4dc4d1
Gosia Magdzik <melassa26@gmail.com>
2024-05-06 20:07:22 +0200

    ...details page without data

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,7 +1,25 @@
+import classes from './page.module';
+import Image from 'next/image';
+
 export default function MealDetailsPage() {
     return <>
-        <header></header>
-        <main></main>
-    </>
+        <header className={classes.header}>
+            <div className={classes.image}>
+                <Image fill />
+            </div>
+            <div className={classes.headerText}>
+                <h1>TITLE</h1>
+                <p className={classes.creator}>
+                    by <a href={`mailto:${'EMAIL'}`}>NAME</a>
+                </p>
+                <p className={classes.summary}>SUMMARY</p>
+            </div>
+        </header>
+        <main>
+            <p className={classes.instructions} dengerouslySetInnerHTML={{
+                __html: '...'
+            }}></p>
+        </main>
+    </>;
     
 }
